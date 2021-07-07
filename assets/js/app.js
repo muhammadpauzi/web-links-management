376b08a09043d6178fd2d82e3e01fbac9fe6bd1f
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-07-07 21:59:20 +0700

    Add feature sort by and update ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,33 +2,18 @@ const inputTitle = document.getElementById('inputTitle');
 const inputUrl = document.getElementById('inputUrl');
 const btnAdd = document.getElementById('btnAdd');
 const listGroup = document.querySelector('.list-group');
-const inputSearch = document.getElementById('inputSearch');;
+const inputSearch = document.getElementById('inputSearch');
+const btnShowFilters = document.querySelector('.btn-show-filters');
+const filters = document.querySelector('.filters');
+const sortLinks = document.getElementById('sortLinks');
 
-const addEventHandle = () => {
-    const body = {
-        id: '000' + Math.floor(Math.random() * 1000) + 1000 + Date.now(),
-        title: inputTitle.value,
-        url: inputUrl.value,
-        created_at: Date.now()
-    }
-    if (!inputTitle.checkValidity()) {
-        return inputTitle.reportValidity();
-    }
-
-    if (!inputUrl.checkValidity()) {
-        return inputUrl.reportValidity();
-    }
-
-    add(body);
-    showList();
-    clearInput();
-}
-
-const clearInput = () => {
-    inputTitle.value = '';
-    inputUrl.value = '';
-}
-
+sortLinks.addEventListener('change', (e) => {
+    showList(null, e.target.value);
+})
+btnShowFilters.addEventListener('click', function () {
+    this.textContent = filters.classList.contains('show') ? 'Show Filters' : 'Hide Filters';
+    filters.classList.toggle('show');
+});
 inputSearch.addEventListener('keyup', (e) => {
     showList(e.target.value);
 });
@@ -41,68 +26,5 @@ document.addEventListener('click', (e) => {
         }
     }
 })
-
-const findAll = () => {
-    if (localStorage.getItem('data') == null) {
-        localStorage.setItem('data', '[]');
-        return false;
-    } else {
-        return JSON.parse(localStorage.getItem('data'));
-    }
-}
-
-const add = body => {
-    const data = findAll();
-    data.push(body);
-    save(data);
-}
-
-const showList = (keyword = null) => {
-    const data = findAll();
-    let temp = '';
-    // Search links
-    if (keyword) {
-        data.map(d => {
-            if (d.title.toLowerCase().includes(keyword.toLowerCase())) {
-                temp += createListItem(d);
-            }
-        });
-        // Message not found
-        if (temp === '') {
-            return listGroup.innerHTML = '<p class="error">Links not found.</p>';
-        }
-    } else {
-        data.map(d => temp += createListItem(d));
-    }
-    // Message not found
-    if (!data || data.length <= 0) {
-        return listGroup.innerHTML = '<p class="error">Links not added yet.</p>';
-    }
-    listGroup.innerHTML = temp;
-}
-
-const deleteData = id => {
-    const data = findAll();
-    const newData = data.filter(d => d.id !== id);
-    save(newData);
-    showList();
-}
-
-const save = data => {
-    localStorage.setItem('data', JSON.stringify(data));
-}
-
-const createListItem = data => {
-    return `<li class="list-item flex ai-c">
-                <div class="flex-1">
-                    <span class="item-title">${data.title}</span>
-                    <span class="item-url">${data.url}</span>
-                </div>
-                <div class="flex">
-                    <a href="${data.url}" class="btn btn-primary btn-visit" id="btnVisit" target="_blank" title="Open link to a new tab"></a>
-                    <button class="btn btn-danger btn-delete" data-id="${data.id}"></button>
-                </div>
-            </li>`;
-}
 
 showList();
---
+++ assets/js/links.js
@@ -0,0 +1,97 @@
+const addEventHandle = () => {
+    const body = {
+        id: '000' + Math.floor(Math.random() * 1000) + 1000 + Date.now(),
+        title: inputTitle.value,
+        url: inputUrl.value,
+        created_at: Date.now()
+    }
+    if (!inputTitle.checkValidity()) {
+        return inputTitle.reportValidity();
+    }
+
+    if (!inputUrl.checkValidity()) {
+        return inputUrl.reportValidity();
+    }
+
+    add(body);
+    showList();
+    clearInput();
+}
+
+const clearInput = () => {
+    inputTitle.value = '';
+    inputUrl.value = '';
+}
+
+const findAll = (sort) => {
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', '[]');
+        return false;
+    } else {
+        const data = JSON.parse(localStorage.getItem('data'));
+        data.sort((a, b) => {
+            console.log(sort)
+            if (sort == "0") { // 0 = by newest
+                return b.created_at - a.created_at;
+            } else if (sort == "1") {
+                return a.created_at - b.created_at;
+            }
+        });
+        return data;
+    }
+}
+
+const add = body => {
+    const data = findAll();
+    data.push(body);
+    save(data);
+}
+
+const showList = (keyword = null, sort = '0') => {
+    const data = findAll(sort);
+    let temp = '';
+    // Search links
+    if (keyword) {
+        data.map(d => {
+            if (d.title.toLowerCase().includes(keyword.toLowerCase())) {
+                temp += createListItem(d);
+            }
+        });
+        // Message not found
+        if (temp === '') {
+            return listGroup.innerHTML = '<p class="error">Links not found.</p>';
+        }
+    } else {
+        data.map(d => temp += createListItem(d));
+    }
+    // Message not found
+    if (!data || data.length <= 0) {
+        return listGroup.innerHTML = '<p class="error">Links not added yet.</p>';
+    }
+    listGroup.innerHTML = temp;
+}
+
+const deleteData = id => {
+    const data = findAll();
+    const newData = data.filter(d => d.id !== id);
+    save(newData);
+    showList();
+}
+
+const save = data => {
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
+const createListItem = data => {
+    return `<li class="list-item flex ai-c">
+                <div class="flex-1">
+                    <span class="item-title">${data.title}</span>
+                    <span class="item-url">${data.url}</span>
+                    <span class="item-created">created at ${new Date(data.created_at).toLocaleString()}</span>
+                </div>
+                <div class="flex">
+                    <a href="${data.url}" class="btn btn-primary btn-visit" id="btnVisit" target="_blank" title="Open link to a new tab"></a>
+                    <button class="btn btn-danger btn-delete" data-id="${data.id}" title="Delete this link"></button>
+                </div>
+            </li>`;
+}
